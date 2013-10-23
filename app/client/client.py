3db4fe2c7605b4e30e87c6a279516aaf9581e167
Andrea Carmisciano <andrea.carmisciano@infoporto.it>
2013-10-23 18:43:59 +0200

    list function added

--- app/client/client.py
+++ app/client/client.py
@@ -23,9 +23,11 @@ while not current:
 
 while True:
 	print colorize("--- MENU ---", 'blue')
-	print "[%s] create user" % colorize("1", 'blue')
+	print "[%s] list credential" % colorize("1", 'blue')
 	print "[%s] save new credential" % colorize("2", 'blue')
 	print "[%s] get password" % colorize("3", 'blue')
+	print "[%s] update password" % colorize("4", 'blue')
+	print "[%s] create user" % colorize("7", 'blue')	
 	print "[%s] debug dump" % colorize("8", 'blue')
 	print "[%s] test server" % colorize("9", 'blue')
 	print "[%s] initialize DB" % colorize("0", 'blue')
@@ -35,25 +37,18 @@ while True:
 		name = raw_input("New DB name: ")
 		print "TODO"
 
-	elif choose == '8':
-		print current
-
-	elif choose == '9':
-		username = raw_input("Insert your username: ")
-		print server.welcome(username)
-	
 	elif choose == '1':
-		username = raw_input("Insert new username: ")
-		password = raw_input("Insert password: ")
-		email = raw_input("Insert e-mail: ")
+		owner = current['username']
+		print "All stored credential for %s" % owner
 
-		res = server.createUser(username, password, email)
-		
-		if not res:
-			print "Unable to create new user"			
-		else:
-			print "User created"
-		
+		res = server.getPassword(False, owner)
+
+		print " TITLE"
+		print "======================="
+		for r in res:
+			print "%s (%s)" % (r['title'], r['username'])
+			
+
 	elif choose == '2':
 		print "creating..."
 		owner = current['username']
@@ -80,9 +75,59 @@ while True:
 			print colorize("Credential created",'green')
 
 	elif choose == '3':
-        #TODO: return keys and not values!!!
 		owner = current['username']
 		title = raw_input("Item title: ")
+		tmpl = "URL: %s \nUsername: %s \nPassword: %s \nNotes: %s"
+		results = server.getPassword(title, owner)
+		if results:
+			for res in results:	
+				values = (colorize(res['url'],'green'), 
+							colorize(res['username'],'green'), 
+							colorize(res['password'],'green'), 
+							colorize(res['note'],'green'))
+				
+				res = tmpl % values
+				print res
+		else:
+			print colorize("No results found.", "fail")
 
-		res = server.getPassword(title, owner)
-		print res
+	elif choose == '4':
+		data = {}
+		results = ''
+		owner = current['username']
+		data['owner'] = owner
+		while not results:
+			title = raw_input("Credential to update: ")
+			results = server.getPassword(title, owner)
+			if not results:
+				print "No credential found. Retry."			
+
+		# new datas
+		data['title']= raw_input("Update the title or press ENTER: ")
+		data['username']= raw_input("Update the username or press ENTER: ")
+		data['password']= raw_input("Update the password or press ENTER: ")
+		data['url']= raw_input("Update the URL or press ENTER: ")
+		data['note']= raw_input("Update the notes or press ENTER: ")		
+
+		result = server.updatePassword(title, owner, data)
+		print result
+	
+	elif choose == '7':
+		username = raw_input("Insert new username: ")
+		password = raw_input("Insert password: ")
+		email = raw_input("Insert e-mail: ")
+
+		res = server.createUser(username, password, email)
+		
+		if not res:
+			print "Unable to create new user"			
+		else:
+			print "User created"
+
+	elif choose == '8':
+		print current
+
+	elif choose == '9':
+		username = raw_input("Insert your username: ")
+		print server.welcome(username)
+		
